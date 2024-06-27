2b12c0382ebe9af2c2aec0479470116bb9e71d87
Theodoro Mota <theolm.mota@gmail.com>
2024-06-27 19:10:37 -0300

    Removing buildSrc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import config.ConfigData
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
--- buildSrc/src/main/java/ConfigData.kt
+++
@@ -1,9 +0,0 @@
-import org.gradle.api.JavaVersion
-
-object ConfigData {
-    const val applicationBundle = "dev.theolm.wwc"
-    const val compileSdkVersion = 34
-    const val minSdkVersion = 24
-    const val targetSdkVersion = 34
-    val javaVersion = JavaVersion.VERSION_17
-}

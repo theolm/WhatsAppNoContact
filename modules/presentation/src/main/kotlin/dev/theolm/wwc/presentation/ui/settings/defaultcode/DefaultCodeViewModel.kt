dacc011a07de0d8216743cd303f31f5bf0c4e1b0
Theo L.M. <theolm.mota@gmail.com>
2024-09-07 16:53:00 -0300

    Adding search field in the country code page (#45)

--- modules/presentation/src/main/kotlin/dev/theolm/wwc/presentation/ui/settings/defaultcode/DefaultCodeViewModel.kt
+++ modules/presentation/src/main/kotlin/dev/theolm/wwc/presentation/ui/settings/defaultcode/DefaultCodeViewModel.kt
@@ -1,12 +1,14 @@
 package dev.theolm.wwc.presentation.ui.settings.defaultcode
 
+import androidx.compose.foundation.text.input.TextFieldState
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dev.theolm.wwc.domain.models.Country
 import dev.theolm.wwc.domain.usecase.ObserveSelectedCountryUseCase
 import dev.theolm.wwc.domain.usecase.UpdateSelectedCountryUseCase
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 class DefaultCodeViewModel(
@@ -14,10 +16,14 @@ class DefaultCodeViewModel(
     private val updateSelectedCountry: UpdateSelectedCountryUseCase,
 ) : ViewModel() {
     private val appSettings: Flow<Country?> = observeSelectedCountry()
-    val uiState: Flow<DefaultCodeUiState> = appSettings.map {
-        DefaultCodeUiState(
-            selectedCountry = it
-        )
+    val uiState = MutableStateFlow(DefaultCodeUiState())
+
+    init {
+        viewModelScope.launch {
+            appSettings.collectLatest {
+                uiState.emit(uiState.value.copy(selectedCountry = it))
+            }
+        }
     }
 
     fun onCountrySelected(country: Country?) {
@@ -28,6 +34,7 @@ class DefaultCodeViewModel(
 }
 
 data class DefaultCodeUiState(
+    val searchField: TextFieldState = TextFieldState(),
     val countries: List<Country> = CountryCodes.codes,
     val selectedCountry: Country? = null,
 )

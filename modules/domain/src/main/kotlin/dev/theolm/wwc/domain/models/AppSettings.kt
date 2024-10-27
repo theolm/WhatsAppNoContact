ec547179ad87a79f8c9bd445fc07b4144a459f80
Theodoro Mota <theolm.mota@gmail.com>
2024-10-27 10:48:27 -0300

    Adding AppSettings version

--- modules/domain/src/main/kotlin/dev/theolm/wwc/domain/models/AppSettings.kt
+++ modules/domain/src/main/kotlin/dev/theolm/wwc/domain/models/AppSettings.kt
@@ -4,6 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class AppSettings(
+    val version: Int = 1,
     val selectedCountryCode: CountryCode? = null,
     val defaultApp: DefaultApp = DefaultApp.WhatsApp
 )

01b7e4c58b4290a068adc6722b8ebf06d5ef92e0
Theodoro Mota <theolm.mota@gmail.com>
2024-10-30 19:44:36 -0300

    Update targetSdk to 35

--- build-logic/src/main/kotlin/config/ConfigData.kt
+++ build-logic/src/main/kotlin/config/ConfigData.kt
@@ -4,8 +4,8 @@ import org.gradle.api.JavaVersion
 
 object ConfigData {
     const val applicationBundle = "dev.theolm.wwc"
-    const val compileSdkVersion = 34
+    const val compileSdkVersion = 35
     const val minSdkVersion = 24
-    const val targetSdkVersion = 34
+    const val targetSdkVersion = 35
     val javaVersion = JavaVersion.VERSION_17
 }

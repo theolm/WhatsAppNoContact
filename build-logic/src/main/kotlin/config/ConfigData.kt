adcab3097471d9a622ddb68115672721488a44f3
Theodoro Mota <theolm.mota@gmail.com>
2025-09-27 12:55:23 -0300

    Update compileSdkVersion and library dependencies

--- build-logic/src/main/kotlin/config/ConfigData.kt
+++ build-logic/src/main/kotlin/config/ConfigData.kt
@@ -4,8 +4,8 @@ import org.gradle.api.JavaVersion
 
 object ConfigData {
     const val applicationBundle = "dev.theolm.wwc"
-    const val compileSdkVersion = 35
+    const val compileSdkVersion = 36
     const val minSdkVersion = 24
-    const val targetSdkVersion = 35
+    const val targetSdkVersion = 36
     val javaVersion = JavaVersion.VERSION_17
 }
